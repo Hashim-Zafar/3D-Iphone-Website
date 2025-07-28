4df4a7551fa9451330f4eff1eb8e063fb6d4b44d
Hashim-Zafar <hashimzafar420@gmail.com>
2025-07-28 15:16:13 +0500

    Layout for 3D Is ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useRef, useState } from "react";
 import Nav from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 function App() {
   return (
     <main>
       <Nav />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   );
 }
---
+++ src/components/ModelView.jsx
@@ -0,0 +1,15 @@
+import { View } from "@react-three/drei";
+
+function ModelView({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  size,
+  item,
+}) {
+  return <div>ModelView</div>;
+}
+
+export default ModelView;
